d030ec816d54a6a9cff53045079612022096a5b1
Minimob <68860403+Minimob-vRP@users.noreply.github.com>
2024-03-18 15:12:18 +0100

    Update CreateMove.cpp

--- src/hooks/functions/CreateMove.cpp
+++ src/hooks/functions/CreateMove.cpp
@@ -9,7 +9,7 @@
 void __fastcall hooks::create_move::hooked(CSGOInput* input, int slot, bool unk, bool sub_tick)
 {
     sdk::sub_tick = sub_tick;
-    sdk::user_cmd = input->GetUserCmd();
+    sdk::user_cmd = input->get_usercmd();
     sdk::base_user_cmd = sdk::user_cmd->m_base_cmd;
 
     if (!g::engine_client->IsConnected() || !g::engine_client->IsInGame() || !entity_data::local_player_pawn || !sdk::user_cmd || !sdk::base_user_cmd)
